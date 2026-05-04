412b0692dc9f8b5737567cfdf2ff8daed72687de
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-05-04 19:23:05 +0300

    Soft

--- Soft.cpp
+++ Soft.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "Soft.h"
+
+
+ Soft::Soft() {
+  name = "undefined";
+  company = "undefined";
+}
+
+ Soft::Soft(string name, string company) {
+   this->name = name;
+     this->company = company;
+}
+
+Soft::~Soft() {
+}
+
+ void Soft::setName(string name) { this->name = name;}
+ void Soft::setCompany(string company) {this->company = company;}
+
+ string Soft::getName() const { return name;}
+string Soft::getCompany() const { return company; }

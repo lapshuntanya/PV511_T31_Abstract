a5afce812fd0aa3b15ffabeed0eabc22fd77047f
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-05-04 20:52:52 +0300

    Admin

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "Free.h"
 #include "Shareware.h"
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+#include "Admin.h"
 
 int main() {
 
@@ -13,7 +14,11 @@ int main() {
     // Shareware d("VS", "Microsoft", Date(), 365);
     // d.print();
 
+    Admin itStep; //load
+    itStep.addSoft( new Shareware("CLion", "JBrains", Date() -1, 3));
+
+    itStep.showList();
+
 
     return 0;
-
 }
